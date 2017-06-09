9ff513f9dbb53dff029e2daf578713d2656f26a5
Oskari Ruutiainen <oskari.ruutiainen@gofore.com>
2017-06-09 23:34:10 +0300

    Starting this project...

---
+++ collector.js
@@ -0,0 +1,12 @@
+"use strict"
+
+var strava = require('strava-v3');
+
+strava.athlete.get({},function(err,payload,limits) {
+    if(!err) {
+        console.log(payload);
+    }
+    else {
+        console.log(err);
+    }
+});
